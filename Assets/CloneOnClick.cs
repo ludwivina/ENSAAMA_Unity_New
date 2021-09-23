298d08d5f701342ee91ade065500ab5f615ad36d
ludwivina <ludvina.adam@gmail.com>
2021-09-23 11:49:02 +0200

    push

--- Assets/CloneOnClick.cs
+++ Assets/CloneOnClick.cs
@@ -6,11 +6,7 @@ public class CloneonClick : MonoBehaviour
 {
     public int numberOfClones = 3;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
+    
 
     // Update is called once per frame
     void Update()
